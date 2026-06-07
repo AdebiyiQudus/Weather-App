3d5d78690d1a63bccd53adc610f2854acb7d11c6
Adebiyi <adebiyiqudus2018@gmail.com>
2026-06-08 00:36:58 +0100

    Bind location

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-// super() initializes the parent React.Component
-// super(props) lets you access this.props inside the constructor
-// constructor is a special method for creating and initializing an object created with a class. In React, it's used to set up the initial state and bind methods.
-// "this" keyword points to the current component instance (actual version of the component displayed on the screen)
 import React from "react";
 
-class Counter extends React.Component {
-  constructor(props) {
+class App extends React.Component {
+  constructor (props) {
     super(props);
-
-    this.state = { count: 5 };
-    // Binding this keyword manually to access  our event functions
-    this.handleDecrement = this.handleDecrement.bind(this);
-    this.handleIncrement = this.handleIncrement.bind(this)
-  }
-    // handlers functions are defined as class methods 
-  handleDecrement() { 
-    this.setState((curState) => {
-      return { count: curState.count - 1 }
-    })
+    this.state = { location: "Moscow"};
+    this.fetchWeather = this.fetchWeather.bind(this);
   }
 
-  handleIncrement() { 
-    this.setState((curState) => {
-      return { count: curState.count + 1 }
-    })
+  fetchWeather() {
+    console.log("Loading data...")
+    console.log(this)
   }
-
   render() {
-    const date = new Date("june 21 2027")
-    date.setDate(date.getDate() + this.state.count)
-
     return (
-      <div>
-        <button onClick={this.handleDecrement}>-</button>
-        <span>
-        {date.toDateString()} [{this.state.count}]
-          </span>
-        <button onClick={this.handleIncrement}>+</button>
+      <div className="app">
+        <h1>Classy Weather</h1>
+        <div>
+      <input
+        type="text"
+        placeholder="Search from location..."
+        value={this.state.location}
+        onChange={(e) => this.setState({location: e.target.value})}
+      />
       </div>
-    );
+      <button onClick={this.fetchWeather}>Get Weather</button>
+      </div>
+    )
   }
 }
 
-export default Counter;
+export default App
---
+++ src/counter.js
@@ -0,0 +1,45 @@
+// super() initializes the parent React.Component
+// super(props) lets you access this.props inside the constructor
+// constructor is a special method for creating and initializing an object created with a class. In React, it's used to set up the initial state and bind methods.
+// "this" keyword points to the current component instance (actual version of the component displayed on the screen)
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+    // Binding this keyword manually to access  our event functions
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this)
+  }
+    // handlers functions are defined as class methods 
+  handleDecrement() { 
+    this.setState((curState) => {
+      return { count: curState.count - 1 }
+    })
+  }
+
+  handleIncrement() { 
+    this.setState((curState) => {
+      return { count: curState.count + 1 }
+    })
+  }
+
+  render() {
+    const date = new Date("june 21 2027")
+    date.setDate(date.getDate() + this.state.count)
+
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+        {date.toDateString()} [{this.state.count}]
+          </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
